6d2b0b51bca087d82af0f51001417220c74de757
Ivan Alles <ivan.alles@gmail.com>
2021-01-19 15:46:58 +0100

    Create class.

--- localizer/camera_demo.py
+++ localizer/camera_demo.py
@@ -1,3 +1,4 @@
+import enum
 import sys
 
 import cv2
@@ -7,40 +8,57 @@ from localizer import predict
 from localizer import utils
 
 
-def run(camera_id):
-    scale_factor = None
+class CameraDemo:
 
-    localizer = predict.Localizer(r'models\.archive\tools_tl-20210118-145200/config.json')
-    object_size = localizer.cfg['object_size']
-    camera = cv2.VideoCapture(camera_id)
+    class Mode(enum.Enum):
+        DETECT = 0
+        TRAIN = 1
 
-    while True:
-        if cv2.waitKey(1) == ord('q'):
-            break
-        ret, camera_image = camera.read()
-        if camera_image is None:
-            print('Cannot read image')
-            continue
+    def __init__(self, camera_id):
+        self._scale_factor = None
+        self._mode = self.__class__.Mode.DETECT
+        self._localizer = predict.Localizer(r'models\.archive\tools_tl-20210118-145200/config.json')
+        self._object_size = self._localizer.cfg['object_size']
+        self._camera = cv2.VideoCapture(camera_id)
 
-        if scale_factor is None:
-            actual_length = np.max(camera_image.shape[:2])
-            desired_length = object_size * 6
-            scale_factor = desired_length / actual_length
+    def run(self):
+        while True:
+            if cv2.waitKey(1) == ord('q'):
+                break
+            ret, camera_image = self._camera.read()
+            if camera_image is None:
+                print('Cannot read image')
+                continue
 
-        camera_image = cv2.resize(camera_image, (0, 0),
-                                  fx=scale_factor,
-                                  fy=scale_factor)
+            if self._scale_factor is None:
+                actual_length = np.max(camera_image.shape[:2])
+                desired_length = self._object_size * 6
+                scale_factor = desired_length / actual_length
+
+            camera_image = cv2.resize(camera_image, (0, 0), fx=scale_factor, fy=scale_factor)
+
+            if self._mode == self.__class__.Mode.DETECT:
+                self._detect(camera_image)
+            else:
+                self._train(camera_image)
+
+            cv2.imshow('camera', camera_image)
+
+
+    def _detect(self, camera_image):
         image = camera_image.astype(np.float32) / 255
-        predictions = localizer.predict(image)
+        predictions = self._localizer.predict(image)
         cv2.circle(camera_image,
                    (camera_image.shape[1] // 2, camera_image.shape[0] // 2),
-                   object_size // 2, (0, 255, 0))
+                   self._object_size // 2, (0, 255, 0))
         utils.draw_objects(camera_image, predictions, axis_length=20, thickness=2)
-        cv2.imshow('camera', camera_image)
 
+    def _train(self, camera_image):
+        pass
 
 
 
 if __name__ == '__main__':
     camera_id = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-    run(camera_id)
+    app = CameraDemo(camera_id)
+    app.run()
