9b19789bf74ed446e6747c024d08e91e640bde8e
Ivan Alles <ivan.alles@gmail.com>
2021-01-21 09:54:04 +0100

    Refactoring.

--- localizer/camera_demo.py
+++ localizer/camera_demo.py
@@ -16,34 +16,40 @@ from localizer import utils
 class CameraDemo:
 
     class Mode(enum.Enum):
-        DETECT = 0
-        TRAIN = 1
+        DETECT = 0  # Detect using current model
+        NEW_MODEL = 1  # Create a new dataset and train a new model
 
     def __init__(self, camera_id):
         self._template_cfg_path = os.path.join(os.path.dirname(__file__), 'camera_demo.json')
         self._model_dir = os.path.join('.temp', 'demo_model')
         self._dataset_path = os.path.join(self._model_dir, 'dataset.json')
-        self._dataset = []
+        self._cfg_path = os.path.join(self._model_dir, 'config.json')
+
+        self._dataset = None
         self._scale_factor = None
         self._mode = self.__class__.Mode.DETECT
-        # self._localizer = predict.Localizer(r'models\.archive\tools_tl-20210118-145200/config.json')
-        self._localizer = predict.Localizer(r"D:\ivan\projects\localizer-transfer-learning\.temp\demo_model\config.json")
-        self._object_size = self._localizer.cfg['object_size']
-        self._camera = cv2.VideoCapture(camera_id)
+        self._localizer = None
+        self._object_size = None
         self._input_image = None
         self._view_image = None
         self._key = -1
         os.makedirs(self._model_dir, exist_ok=True)
+
+        self._camera = cv2.VideoCapture(camera_id)
+        self._load_model()
+
 
     def run(self):
         while True:
             self._key = cv2.waitKey(1)
             if self._key == ord('q'):
                 break
-            elif self._key == ord('t'):
-                self._mode = self.__class__.Mode.TRAIN
+            elif self._key == ord('n'):
+                self._mode = self.__class__.Mode.NEW_MODEL
             elif self._key == ord('d'):
                 self._mode = self.__class__.Mode.DETECT
+            elif self._key == ord('R'):
+                self._train()  # Retrain model on existing data, useful for tests.
 
             ret, camera_image = self._camera.read()
             if camera_image is None:
@@ -63,7 +69,7 @@ class CameraDemo:
             if self._mode == self.__class__.Mode.DETECT:
                 self._detect()
             else:
-                self._train()
+                self._new_model()
 
             cv2.imshow('camera', self._view_image)
 
@@ -72,12 +78,18 @@ class CameraDemo:
         predictions = self._localizer.predict(input)
         utils.draw_objects(self._view_image, predictions, axis_length=20, thickness=2)
 
-    def _train(self):
+    def _load_model(self):
+        self._localizer = predict.Localizer('.temp/demo_model/config.json')
+        self._object_size = self._localizer.cfg['object_size']
+        self._mode = self.__class__.Mode.DETECT
+
+    def _new_model(self):
+        self._dataset = []
         x = self._input_image.shape[1] / 2
         y = self._input_image.shape[0] / 2
         cv2.circle(self._view_image, (int(x), int(y)), self._object_size // 2, (0, 255, 0))
         if self._key == ord(' '):
-            image_file = datetime.datetime.now().strftime('image-%Y%m%d-%H%M%S-%f.png')
+            image_file = datetime.datetime.now().strftime('image-1.png')
             image_path = os.path.join(self._model_dir, image_file)
             cv2.imwrite(image_path, self._input_image)
             data_element = {
@@ -97,13 +109,16 @@ class CameraDemo:
             with open(self._dataset_path, 'w') as f:
                 json.dump(self._dataset, f, indent=' ')
 
-            cfg_path = os.path.join(self._model_dir, 'config.json')
-            shutil.copyfile(self._template_cfg_path, cfg_path)
+            shutil.copyfile(self._template_cfg_path, self._cfg_path)
 
-            train.configure_logging(cfg_path)
-            trainer = train.Trainer(cfg_path)
-            trainer.run()
-            self._mode = self.__class__.Mode.DETECT
+            self._train()
+
+    def _train(self):
+        train.configure_logging(self._cfg_path)
+        trainer = train.Trainer(self._cfg_path)
+        trainer.run()
+
+        self._load_model()
 
 
 if __name__ == '__main__':
