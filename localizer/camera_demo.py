c8f79a2c44e32482d43c89726363d4ab3028cee7
Ivan Alles <ivan.alles@gmail.com>
2021-01-25 17:24:33 +0100

    Train on 5 images.

--- localizer/camera_demo.py
+++ localizer/camera_demo.py
@@ -31,6 +31,7 @@ class CameraDemo:
         self._object_size = None
         self._camera_image = None
         self._view_image = None
+        self._image_idx = 0
         self._key = -1
         os.makedirs(self._model_dir, exist_ok=True)
 
@@ -45,6 +46,8 @@ class CameraDemo:
                 break
             elif self._key == ord('n'):
                 self._mode = self.__class__.Mode.NEW_MODEL
+                self._image_idx = 0
+                self._dataset = []
             elif self._key == ord('d'):
                 self._mode = self.__class__.Mode.DETECT
             elif self._key == ord('r'):
@@ -72,21 +75,19 @@ class CameraDemo:
 
             cv2.imshow('camera', self._view_image)
 
-    def _draw_pose(self, image, x, y, angle, color=(0, 255, 0), draw_object_size=True):
+    def _draw_pose(self, x, y, angle, color=(0, 255, 0)):
         arrow = np.array([0, 0, 0, -1, 0, -1, .1, -.8, 0, -1, -.1, -0.8]).reshape(-1, 2)
         t = utils.make_transform2(self._object_size / 2, angle, x, y)
         arrow = np.dot(np.append(arrow, np.ones((arrow.shape[0], 1)), axis=1), t.T)[:, :2].astype(int)
         for i in range(0, len(arrow), 2):
-            cv2.line(image, tuple(arrow[i]), tuple(arrow[i + 1]), color)
-        if draw_object_size:
-            cv2.circle(self._view_image, (int(x), int(y)), self._object_size // 2, color)
+            cv2.line(self._view_image, tuple(arrow[i]), tuple(arrow[i + 1]), color, thickness=2)
+        cv2.circle(self._view_image, (int(x), int(y)), self._object_size // 2, color, thickness=2)
 
     def _detect(self):
         input = self._make_input(self._camera_image)
-        predictions = self._localizer.predict(input)
-        utils.draw_objects(self._view_image, predictions, axis_length=20, thickness=2)
-        for object in predictions:
-            self._draw_pose(self._view_image, object.origin[0], object.origin[1], object.angle)
+        objects = self._localizer.predict(input)
+        for obj in objects:
+            self._draw_pose(obj.origin[0], obj.origin[1], obj.angle)
 
     def _make_input(self, image):
         image = image.astype(np.float32) / 255
@@ -99,12 +100,19 @@ class CameraDemo:
         self._mode = self.__class__.Mode.DETECT
 
     def _new_model(self):
-        self._dataset = []
-        x = self._camera_image.shape[1] / 2
-        y = self._camera_image.shape[0] / 2
-        cv2.circle(self._view_image, (int(x), int(y)), self._object_size // 2, (0, 255, 0))
+        s = self._camera_image.shape
+        positions = [
+                [s[1] / 2, s[0] / 2],
+                [self._object_size, self._object_size],
+                [s[1] - self._object_size, self._object_size],
+                [s[1] - self._object_size, s[0] - self._object_size],
+                [self._object_size, s[0] - self._object_size],
+            ]
+
+        angle = 2 * np.pi / len(positions) * self._image_idx
+        self._draw_pose(*positions[self._image_idx], angle)
         if self._key == ord(' '):
-            image_file = datetime.datetime.now().strftime('image-1.png')
+            image_file = datetime.datetime.now().strftime(f'image-{self._image_idx}.png')
             image_path = os.path.join(self._model_dir, image_file)
             input_image = self._make_input(self._camera_image) * 255
             cv2.imwrite(image_path, input_image)
@@ -114,20 +122,20 @@ class CameraDemo:
                    {
                     "category": 0,
                     "origin": {
-                     "x": x,
-                     "y": y,
-                     "angle": 0
+                     "x": positions[self._image_idx][0],
+                     "y": positions[self._image_idx][1],
+                     "angle": angle
                     }
                    }
                 ]
             }
             self._dataset.append(data_element)
-            with open(self._dataset_path, 'w') as f:
-                json.dump(self._dataset, f, indent=' ')
-
-            shutil.copyfile(self._template_cfg_path, self._cfg_path)
-
-            self._train()
+            self._image_idx += 1
+            if self._image_idx == len(positions):
+                with open(self._dataset_path, 'w') as f:
+                    json.dump(self._dataset, f, indent=' ')
+                shutil.copyfile(self._template_cfg_path, self._cfg_path)
+                self._train()
 
     def _train(self):
         train.configure_logging(self._cfg_path)
